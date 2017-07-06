6287aaffb141c7275e6deb47ec31f2be6dca68a3
abhijit <abhijit@administrator>
2017-07-06 13:28:31 +0530

    added add functionality

---
+++ scientificCalculator/src/main.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function calculator(){
+    var a= 4;
+    var b = 5;
+    this.add = function()
+    {
+        alert(a+b);
+    }
+    
+}
+calculator.add();
